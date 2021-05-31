a68ffcbba4dcdd230e96ae2adb9d71335e72e80c
Lightraine2 <laurencedale1986@gmail.com>
2021-05-31 21:37:25 +0100

    home button not working 59.26

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,11 +6,21 @@ import {
   Pressable
 } from 'react-native'
 import styles from './styles'
+import Fontisto from 'react-native-vector-icons/Fontisto'
 
 const HomeScreen = (props) => {
   return (
     <View>
-      {/* search bar */}
+
+      <Pressable 
+      style={styles.searchButton} 
+      onPress={() => console.warn('search button press')}
+      > 
+      <Fontisto name="search" size={25} color={"#f15454"} />
+        <Text style={styles.searchButtonText}>
+          Where are you going?</Text>
+      </Pressable>
+
       <ImageBackground source={require('../../../assets/images/wallpaper.jpg')} 
       style={styles.image} 
       >
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
 
@@ -33,7 +33,29 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold'
 
-  }
+  },
+
+  searchButton: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 20,
+    zIndex: 100,
+
+
+  },
+
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold'
+
+  },
 
 });
 
