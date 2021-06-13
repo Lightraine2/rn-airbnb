4bed098d5232521646de1b1d015456ed93c23194
Lightraine2 <laurencedale1986@gmail.com>
2021-06-13 07:59:20 +0100

    search destinations 2.12

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, FlatList, Text} from 'react-native';
 import styles from './styles'
+import Entypo from 'react-native-vector-icons/Entypo'
+
+import searchResults from '../../../assets/data/search'
 
 const DestinationSearchScreen = (props) => {
+
+  const [inputText, setInputText] = useState('');
+
   return (
-    <View>
-      <Text>A component</Text>
+    <View style={styles.container}>
+      
+      {/* input component */}
+      <TextInput 
+      style={styles.textInput}
+      placeholder="Where are you going?"
+      value={inputText}
+      onChangeText={setInputText}
+      />
+      
+      {/* list of destinations */}
+      <FlatList
+      data={searchResults}
+      renderItem={(({item}) => (
+      
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Entypo name={"location-pin"} size={30} />
+        </View>
+      <Text style={styles.locationText}>{item.description}</Text>
+      </View>
+      
+      )
+      )}
+      />
+
     </View>
   );
 };
---
+++ src/screens/DestinationSearch/styles.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 20,
+    },
+    textInput: {
+        marginTop: 20,
+        fontSize: 20, 
+        marginBottom: 20  
+    },
+    row: { 
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 15,
+        borderBottomWidth: 1,
+        borderColor: 'lightgrey'
+
+    },
+    iconContainer: {
+        backgroundColor: '#d4d4d4d4',
+        padding: 7,
+        borderRadius: 10,
+        marginRight: 10
+
+    },
+    locationText: {
+
+    }
+});
+
+export default styles;
